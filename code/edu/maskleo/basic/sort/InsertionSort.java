cfe70671d5f613e9950c8072755d630e00b971cd
maskleo <masque@aliyun.com>
2018-05-16 22:30:37 +0800

    添加插入排序

---
+++ code/edu/maskleo/basic/sort/InsertionSort.java
@@ -0,0 +1,19 @@
+package edu.maskleo.basic.sort;
+
+import static edu.maskleo.basic.SortTestHelper.generateRandomArray;
+import static edu.maskleo.basic.SortTestHelper.print;
+
+/**
+ * 插入排序:
+ */
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        int[] array = generateRandomArray(100000, 5000);
+        print(array);
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < array.length; i++) {
+
+        }
+    }
+}
